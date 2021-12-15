829766838666f0c075938a375b7ec77fe3548162
newjackDev <firejet7777@gmail.com>
2021-12-16 00:54:00 +0900

    delete-account-process-회원탈퇴 오류 수정

--- PHP/delete_account_pc.php
+++ PHP/delete_account_pc.php
@@ -8,13 +8,15 @@ include_once("./connect_db.php");
 $sql = "DELETE FROM info WHERE info.index = {$_SESSION['index']}";
 $result = mysqli_query($db, $sql) or die(mysqli_error($db));
 
-$sql = "DELETE FROM notice WHERE author = {$_SESSION['user_id']}";
+$sql = "DELETE FROM notice WHERE notice.author = '{$_SESSION['user_id']}'";
 $result = mysqli_query($db, $sql) or die(mysqli_error($db));
 
-$sql = "DELETE FROM archive WHERE author = {$_SESSION['user_id']}";
+$sql = "DELETE FROM archive WHERE archive.author = '{$_SESSION['user_id']}'";
 $result = mysqli_query($db, $sql) or die(mysqli_error($db));
 
-if ($result === false) {
+session_destroy();
+
+if ($result == false) {
 ?>
 <script>
 alert("회원탈퇴 실패, 관리자에게 문의하세요.");
@@ -23,7 +25,6 @@ location.href = "../index.php"
 <?php
     echo mysqli_error($db);
 } else {
-    session_destroy()
 ?>
 <script>
 alert("회원탈퇴 성공");
